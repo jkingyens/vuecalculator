218eee54c0fabe8c3fa9b3af69f068b722d9827f
Jeff Kingyens <jeff.kingyens@gmail.com>
2021-02-28 19:35:59 -0800

    first

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Calculator></Calculator>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+
+h1{
+  font-size: 3em;
+}
+
+img{
+  max-width: 50px;
+  max-height: 80px;
+  height: 80px;
+}
+</style>
---
+++ src/components/Display.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="display">
+      <div id="displayText">
+        <div id = "leftOf">{{displayValue}}</div>
+      </div>
+      <div id="subText"><p>{{subDisplayValue}}</p></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'display',
+  props: {
+      displayValue: String,
+      subDisplayValue: String
+  },
+  components:{
+  },
+  mounted:function(){
+    this.onLoady()
+  },
+  methods: {
+    onLoady: function() {
+    }
+  }
+  
+}
+</script>
+
+<style >
+@import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+
+#display {
+  width: 100%;
+  border-radius: 5px;
+  height: 200px;
+  float: right;
+}
+
+#leftOf{
+  width: 99.75%;
+}
+
+#blinker{
+  width: 1.00%;
+}
+
+#displayText,  #subText{
+    font-family: 'Lato', sans-serif;
+    text-align: right;
+    margin-bottom: 0px;
+    overflow:hidden;
+    text-overflow: ellipsis;
+}
+#displayText{
+    display: flex;
+    flex-direction: row;
+    height: 100px;
+    font-size: 3em;
+}
+
+#subText{
+    height: 100px;
+    font-size: 1.5em;
+    color: #38908f;
+    border: none;
+    background-color: #f9f9f9;
+}
+</style>
